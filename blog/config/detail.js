f71de83ae25bc31680248b48500ff056f20fade0
q2577873863 <zzehao0613@gmail.com>
2020-12-05 19:32:56 +0800

    test
    test

--- blog/config/detail.js
+++ blog/config/detail.js
@@ -1,5 +1,4 @@
 import electronNote from './detail/electron_note';
-console.log(electronNote);
 export const details = [
   electronNote,
   ''
--- blog/config/detail/electron_note.js
+++ blog/config/detail/electron_note.js
@@ -1,22 +1,24 @@
-import { heredoc } from '../../../src/utils/utils';
-
-export default heredoc(function(){
-/*
+export default `
 ## 二级标题
 ### 三级标题
 #### 四级标题
 
-``` js
+-= js
 // js高亮测试
 const a = document.getElementById("a");
 console.log(a); 
-```
+-=
 
-``` html
+-= html
 <!-- html高亮测试 -->
 <div class="div1">
   <span id="span1">bbbb</span>
 </div>
-```
-*/
-});
+-=
+
+|  表头   | 表头  |
+|  ----  | ----  |
+| 单元格  | 单元格 |
+| 单元格  | 单元格 |
+
+`
