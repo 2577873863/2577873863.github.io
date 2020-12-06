8a5e940696a6b36a5e5f28cba935eeed16c3cf66
q2577873863 <zzehao0613@gmail.com>
2020-12-06 19:20:53 +0800

    change text
    change text

--- blog/config/list.js
+++ blog/config/list.js
@@ -1,7 +1,7 @@
 export const list = [
   {
     id: 0,
-    title: "electron学习击毙",
+    title: "electron学习笔记",
     time: "2020-03-23",
     author: "undefined",
     photoUrl: "http://r.photo.store.qq.com/psc?/V1021XMO1TUVn3/45NBuzDIW489QBoVep5mcVyAsy.wCAIsnBjM5JiTFhbK*BglRYnp4LLnz5ALzU.6yUW7EQkKpfNEf9*TTgEBiDGZR7ETiFh9E0v4M62I8Pg!/r",
